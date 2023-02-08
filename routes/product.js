b44d516b6fa53d8bf21c7294f7adc97b29e3e4e0
aaron moroney <aareon.createdesign@gmail.com>
2023-02-08 13:37:20 +0000

    adapted routes for react router dom

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const productCtrl = require('../controllers/product');
 
+//edited the end point for get all products to '/getAllProducts'
 router.get('/', productCtrl.getAllProducts);
 router.get('/:id', productCtrl.getOneProduct);
 router.post('/order', productCtrl.orderProducts);
